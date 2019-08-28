025f4e348faa9567cc698786f8ea448f4be05a66
xin.jin <xin.jin@kaiostech.com>
2019-08-28 14:15:11 +0800

    Updates

---
+++ precache-manifest.b57c186cf283835ea19883cec7ee4239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4513fd4e87787a7d608a",
+    "url": "/react-bootstrap-example/static/js/runtime~main.cb59f39d.js"
+  },
+  {
+    "revision": "d3174fa43e0bae467316",
+    "url": "/react-bootstrap-example/static/js/main.b904d61d.chunk.js"
+  },
+  {
+    "revision": "ef897dbde08e6ac082fb",
+    "url": "/react-bootstrap-example/static/js/2.b17b7b7d.chunk.js"
+  },
+  {
+    "revision": "d3174fa43e0bae467316",
+    "url": "/react-bootstrap-example/static/css/main.164d8df1.chunk.css"
+  },
+  {
+    "revision": "ef897dbde08e6ac082fb",
+    "url": "/react-bootstrap-example/static/css/2.8edd4572.chunk.css"
+  },
+  {
+    "revision": "23e4c253d3a6e7e75ebf62b4842f5e7a",
+    "url": "/react-bootstrap-example/index.html"
+  }
+];
